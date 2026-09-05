e8ad0c2a21cf7710b53f103ec9942be590e9f707
JiaKangL <E1155649@u.nus.edu>
2026-09-05 09:39:53 +0000

    amend slurm to batch env jobs

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -2,7 +2,7 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=2:00:00   # walltime
+#SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1   # number of CPUs for this task
@@ -13,6 +13,11 @@
 #SBATCH -e rplhighpass-sort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -21,3 +26,6 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
